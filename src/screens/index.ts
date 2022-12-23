e619aca592280025efb6a0c5debc4d02bf889909
Thiago Saraiva <59667699+thiago5araiva@users.noreply.github.com>
2022-12-23 19:58:50 -0300

    create react-native.config.js and update somente navigation & screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,4 +2,5 @@ import HomeScreen from './Home/view';
 import LoginScreen from './Login/view';
 import SignupScreen from './Signup/view';
 import StartScreen from './Start/view';
+
 export { StartScreen, LoginScreen, SignupScreen, HomeScreen };
